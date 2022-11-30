0a0881255bfbe1473cd6bc115e6f4d4fec846624
Alex Law <alexlaw528@gmail.com>
2022-11-30 14:26:33 -0800

    Small CSS updates

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -23,33 +23,35 @@ const Home = () => {
 
 
   return (
-    <div className="container home-page" id="nav-home">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters 
-            letterClass={letterClass}
-            strArray={nameArray}
-            idx={15} 
-          />
-          &nbsp;
-          <AnimatedLetters 
-            letterClass={letterClass}
-            strArray={nameArray2}
-            idx={19} 
-          />
-          <br />
-          <AnimatedLetters letterClass={letterClass}
-            strArray={jobArray}
-            idx={22} 
-          />
-          &nbsp;
-          <AnimatedLetters letterClass={letterClass}
-            strArray={jobArray2}
-            idx={30} 
-          />
-        </h1>
-        <h2> Engineer / Fullstack / Something </h2>
-        <Link to="/contact" className="flat-button">CONTACT ME</Link>
+    <div className="home-page-container">
+      <div className="container home-page" id="nav-home">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters 
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15} 
+            />
+            &nbsp;
+            <AnimatedLetters 
+              letterClass={letterClass}
+              strArray={nameArray2}
+              idx={19} 
+            />
+            <br />
+            <AnimatedLetters letterClass={letterClass}
+              strArray={jobArray}
+              idx={22} 
+            />
+            &nbsp;
+            <AnimatedLetters letterClass={letterClass}
+              strArray={jobArray2}
+              idx={30} 
+            />
+          </h1>
+          <h2> Engineer / Designer / Developer </h2>
+          <Link to="/contact" className="flat-button">CONTACT ME</Link>
+        </div>
       </div>
     </div>
   )
